2c4683f03bad34d9a1971c67ee4740b016baa880
nandiniganeshs1 <>
2021-05-25 12:02:21 +0000

    add scanf statement and swap the values from the user.

--- Program_5.c
+++ Program_5.c
@@ -10,8 +10,11 @@
 int main()
 {
     int a, b, temp;
+    printf("enter values of a and b");
     scanf("%d%d", &a, &b);
-
+    temp=a;
+    a=b;
+    b=temp;
     //Write your code here
     
     
